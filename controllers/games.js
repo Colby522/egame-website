2eed375a8f1e4203dd4592c988f25275f369e1f8
Colby Rothwell <crothwell522@gmail.com>
2025-02-25 15:27:12 -0600

    sends user to games index after signing in

--- controllers/games.js
+++ controllers/games.js
@@ -4,3 +4,12 @@ const router = express.Router()
 const User = require('../models/user.js')
 
 module.exports = router
+
+router.get('/', async (req, res) => {
+    try {
+        res.render('games/index.ejs')
+    } catch (error) {
+        console.log(error)
+        res.redirect('/')
+    }
+})
