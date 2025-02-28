4220509e22c90d072561925cb92bc53e02c8353b
Colby Rothwell <crothwell522@gmail.com>
2025-02-28 16:34:09 -0600

    added pruchase functionality and purchase.ejs view

--- controllers/shop.js
+++ controllers/shop.js
@@ -14,4 +14,31 @@ router.get('/:userId/:gameId', async (req, res) => {
     res.render('shop/show.ejs', {game: chosenGame, user: chosenUser})
 })
 
+router.get('/:userId/:gameId/purchase', async (req, res) => {
+    const chosenUser = await User.findById(req.params.userId)
+    const chosenGame = await chosenUser.games.id(req.params.gameId)
+    res.render('shop/purchase.ejs', {game: chosenGame, user: chosenUser})
+})
+
+router.put('/:userId/:gameId', async (req, res) => {
+    try {
+        const currentUser = await User.findById(req.session.user._id)
+        const chosenUser = await User.findById(req.params.userId)
+        const chosenGame = await chosenUser.games.id(req.params.gameId)
+        if (chosenGame.forSale === 'on') {
+            chosenGame.forSale = true
+        } else {
+            chosenGame.forSale = false
+        }
+        chosenGame.forSale = false
+        await chosenUser.save()
+        currentUser.games.push(chosenGame)
+        await currentUser.save()
+        res.redirect(`/users/${currentUser._id}/games`)
+    } catch (error) {
+        console.log(error)
+        res.redirect('/')
+    }
+})
+
 module.exports = router
